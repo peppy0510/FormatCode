7fef4665a47dfa8d45d03c14d5dc99504ff29d83
Taehong Kim <peppy0510@hotmail.com>
2020-08-06 10:13:23 +0900

    first commit

---
+++ FormatCode.py
@@ -0,0 +1,114 @@
+# encoding: utf-8
+
+
+'''
+author: Taehong Kim
+email: peppy0510@hotmail.com
+
+requirements:
+
+description:
+
+reference:
+
+'''
+
+
+import sublime  # noqa
+import sublime_plugin
+
+import os
+
+
+SYNTAX_DICTIONARY = {
+    'java': ['Java'],
+    'c': ['C', 'C++', 'C#'],
+    'typescript': ['TypeScript', 'TypeScriptReact'],
+    'css': ['CSS', 'CSS (Django)', 'SCSS', 'naomi.css3'],
+    'json': ['JSON', 'JSON (Sublime)', 'JSON Key-Value'],
+    'markdown': ['Markdown', 'Markdown GFM', 'MultiMarkdown'],
+    'javacript': ['JSX', 'JavaScript', 'JavaScriptNext', 'JavaScript (Babel)'],
+    'python': ['Python', 'Python3', 'Python Django', 'MagicPython'],
+    'html': ['HTML', 'HTML (Django)', 'HTML-extended', 'XML',
+             'NgxHTML', 'naomi.html5', 'HTML (Underscore)'],
+}
+
+
+class Syntax:
+
+    def __contains__(self, name):
+        for key in self.keywords:
+            dictionary = SYNTAX_DICTIONARY.get(key)
+            if dictionary and name in dictionary:
+                return True
+        return False
+
+    def syntaxmap(self, *keywords):
+        self.keywords = keywords
+        return self
+
+
+syntaxmap = Syntax().syntaxmap
+
+
+class FormatCodeCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+
+        viewport_position = self.view.viewport_position()
+
+        syntax = self.view.settings().get('syntax')
+        syntax = os.path.splitext(os.path.split(syntax)[-1])[0]
+        extension = os.path.splitext(self.view.file_name())[-1].strip('.')
+
+        print("{}({{'syntax': '{}', 'extension': '{}'}})".format(
+            self.__class__.__name__[:-7], syntax, extension))
+
+        if syntax in syntaxmap('c', 'java', 'json', 'html', 'css', 'python', 'javascript', 'typescript'):
+            self.view.run_command('unexpand_tabs')
+            self.view.run_command('set_setting', {'setting': 'tab_size', 'value': 4})
+            self.view.run_command('expand_tabs', {'set_translate_tabs': True})
+            self.view.run_command('expand_tabs', {'translate_tabs_to_spaces': True})
+            # self.view.run_command('re_indent_to_four')
+            # self.view.run_command('indentation_convert_to_spaces')
+            # "detect_indentation": true,
+            # "tab_size": 4,
+            # "translate_tabs_to_spaces": false
+
+        if syntax in syntaxmap('c', 'java', 'html', 'css', 'javascript', 'typescript'):
+            self.view.run_command('reindent', {"single_line": False})
+
+        if syntax in syntaxmap('json') or extension in (
+                'json', 'sublime-settings', 'sublime-commands', 'sublime-keymap',
+                'htmlhint.htmlhintrc', 'sublime-menu', 'jsbeautifyrc', 'hidden-color-scheme'):
+            self.view.run_command('js_prettier')
+            # self.view.set_syntax_file('JSON Key-Value')
+            # self.view.run_command('set_syntax', 'JSON Key-Value')
+            # self.view.run_command('pretty_json')
+
+        if syntax in syntaxmap('html') or extension in ('htm', 'html',):
+            self.view.run_command('htmlprettify')
+
+        if syntax in syntaxmap('javacript') or extension in ('js', 'jsx',):
+            # self.view.run_command('sort_js_imports')
+            self.view.run_command('javascript_fix_imports')
+            self.view.run_command('js_prettier')
+
+        if syntax in syntaxmap('css') or extension in ('css', 'scss', 'sass',):
+            self.view.run_command('css_comb')
+
+        if syntax in syntaxmap('python') or extension in ('py', 'pyw',):
+            self.view.run_command('python_fiximports')
+            self.view.run_command('pep8_autoformat')
+
+        if syntax in syntaxmap('typescript') or extension in ('ts',):
+            self.view.run_command('typescript_format_document')
+            self.view.run_command('typescript_organize_imports')
+
+        if syntax in syntaxmap('c', 'java') or extension in ('cpp', 'java',):
+            self.view.run_command('astyleformat')
+
+        if syntax in syntaxmap('markdown') or extension in ('md',):
+            self.view.run_command('markdown_table_format')
+
+        self.view.set_viewport_position(viewport_position)
---
+++ README.md
@@ -0,0 +1,16 @@
+## FormatCode for SublimeText
+
+확장자에 따른 여러가지 코드 포맷 패키지를 하나의 단축키로 사용할 수 있도록 도와주는 패키지입니다. 사전에 다음과 같은 패키지들의 설치가 필요합니다.
+
+
+for JavaScript, TypeScript, CSS, SCSS, HTML
+
+`JsPrettier`, `HTML-CSS-JS Prettify`, `CSScomb`, `CSScomb`, ...
+
+for Python
+
+`Python PEP8 Autoformat`, ...
+
+for Markdown
+
+`Markdown Table Formatter`, ...
